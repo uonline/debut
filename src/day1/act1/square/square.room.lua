91dde170ea3acbb6414a1cf898dffbcc37c27848
m1kc (Max Musatov) <m1kc@yandex.ru>
2016-08-22 20:36:37 +0300

    Rehaul project structure

---
+++ src/day1/act1/square/square.room.lua
@@ -0,0 +1,17 @@
+square = room {
+	nam = 'Центральная площадь';
+	dsc = [[
+		Ты стоишь на центральной площади деревни. Ещё совсем светло, и людей почти
+		нет.
+	]];
+	obj = {
+		'square_well';
+		'elder';
+		'blacksmith';
+	};
+	way = {
+		'home',
+		'forest',
+		'village_neib'
+	};
+}
---
+++ src/main.room.lua
@@ -0,0 +1,9 @@
+main = room {
+	nam = 'Стартовый экран';
+	dsc = [[
+		TODO TODO TODO TODO
+	]];
+	way = {
+		'home';
+	};
+}
