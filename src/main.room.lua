d0b4ff94ae2da145f661f7b5037a5e72d35a8a78
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-04-11 12:12:32 +0300

    [D2A1] Add a first draft

--- src/main.room.lua
+++ src/main.room.lua
@@ -8,5 +8,6 @@ main = room {
 		vroom('День 1, акт 2', 'forest_edge');
 		vroom('День 1, акт 3', 'lords_lands');
 		vroom('День 1, акт 4', 'uh_prison');
+		vroom('День 2, акт 1', 'your_cell');
 	};
 }
