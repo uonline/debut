bdf02a2beee192f45dad619feb246a4ac807c5dc
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-04-12 14:48:46 +0300

    [D1A5] Add some decorations

--- src/main.room.lua
+++ src/main.room.lua
@@ -8,6 +8,7 @@ main = room {
 		vroom('День 1, акт 2', 'forest_edge');
 		vroom('День 1, акт 3', 'lords_lands');
 		vroom('День 1, акт 4', 'uh_prison');
+		vroom('День 1, акт 5', 'tract_camp');
 		vroom('День 2, акт 1', 'your_cell');
 	};
 }
