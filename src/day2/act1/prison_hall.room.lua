55505f926d60d9aeba9a0fbf45af9bb11cdd16f2
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-07-19 01:14:35 +0300

    [D2A1] Add some logic

--- src/day2/act1/prison_hall.room.lua
+++ src/day2/act1/prison_hall.room.lua
@@ -1,3 +1,11 @@
+_guard_just_died = false
+
+prison_hall_to_prison_guards_room = vroom('Комната стражи', 'prison_guards_room');
+prison_hall_to_prison_guards_room:disable();
+
+prison_hall_to_barracks_hall = vroom('Коридор оружейной', 'barracks_hall');
+prison_hall_to_barracks_hall:disable();
+
 prison_hall = room {
 	nam = 'Тюремный коридор';
 	dsc = [[
@@ -5,11 +13,72 @@ prison_hall = room {
 		По бокам тянется длинный ряд камер -- сейчас они все пусты.
 	]];
 	obj = {
-		-- ^_^
+		'prison_hall_guard';
+		'prison_hall_table';
+		'prison_hall_cup';
 	};
 	way = {
 		'your_cell';
-		'prison_guards_room';
-		'barracks_hall';
+		prison_hall_to_prison_guards_room;
+		prison_hall_to_barracks_hall;
 	};
+}
+
+prison_hall_guard = obj {
+	nam = 'Стражник';
+	dsc = function()
+		if _guard_just_died then
+			return [[
+				У твоих ног лежит труп стражника.
+			]]
+		end
+		return [[
+			В десяти шагах от тебя {стражник} внимательно изучает план
+			эвакуации при пожаре.
+		]]
+	end;
+	act = [[
+		Ты внимательно и с некоторым сочувствием рассматриваешь стражника.
+		Его жизнь, должно быть, действительно очень скучна.
+	]];
+	used = function(self, what)
+		if what == prison_hall_cup then
+			_guard_just_died = true
+			prison_hall_to_prison_guards_room:enable()
+			inv():del 'prison_hall_cup'
+			return [[
+				Ещё раз прокрутив в голове все планы, в тысячный раз пересчитав
+				все комбинации и найдя наконец оптимальную комбинацию факторов,
+				ты решаешь привести свой гениальный план в действие. Итак,
+				ты подкрадываешься к стражнику и хуяришь его кружкой по темени.
+				^
+				Теперь надо куда-то оттащить труп. Может, вон за ту железную
+				дверь?
+			]]
+		end
+	end;
+}
+on_event('guard body is hidden', function()
+	prison_hall_guard:disable()
+end)
+
+prison_hall_table = obj {
+	nam = 'Стол';
+	dsc = [[
+		Рядом с тобой стоит стол.
+	]];
+}
+
+prison_hall_cup = obj {
+	nam = 'Медная кружка';
+	dsc = [[
+		На нём стоит {тяжёлая медная кружка}.
+	]];
+	tak = [[
+		Ты берёшь кружку в руки и начинаешь строить в уме коварные
+		многоходовки.
+	]];
+	inv = [[
+		Ты взвешиваешь кружку в руке. Килограмм! Нет, два!
+	]];
 }
