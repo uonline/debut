38f8e0568547385a97261c1f45fb1b46942782f5
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-07-26 15:24:58 +0300

    [D1A3] Add some logic

--- src/day2/act3/regimenstrasse.room.lua
+++ src/day2/act3/regimenstrasse.room.lua
@@ -8,9 +8,67 @@ regimenstrasse = room {
 		весь булыжник тут окрашен в жёлтый цвет.
 	]];
 	obj = {
-		-- ^_^
+		'regimenstrasse_propagandist';
+		'regimenstrasse_singer';
 	};
 	way = {
 		'berlinstrasse';
 	};
+}
+
+on_event('regimenstrasse belongs to singer', function()
+	objs('regimenstrasse'):del('regimenstrasse_propagandist');
+	objs('regimenstrasse'):del('regimenstrasse_singer');
+
+	objs('regimenstrasse'):add('regimenstrasse_singer_silent');
+end)
+
+on_event('regimenstrasse belongs to propagandist', function()
+	objs('regimenstrasse'):del('regimenstrasse_propagandist');
+	objs('regimenstrasse'):del('regimenstrasse_singer');
+
+	objs('regimenstrasse'):add('regimenstrasse_propagandist_silent');
+end)
+
+regimenstrasse_propagandist = obj {
+	nam = 'Глашатай Благих';
+	dsc = [[
+		{Какой-то лысый чёрт} орёт в рупор про спасение души. Люди бросают ему
+		в ермолку монетки.
+	]];
+	act = function()
+		walk 'regimenstrasse_conflict'
+	end;
+}
+
+regimenstrasse_propagandist_silent = obj {
+	nam = 'Глашатай Благих';
+	dsc = [[
+		{Глашатай Благих} орёт в рупор про спасение души. Люди бросают ему
+		в ермолку монетки.
+	]];
+	act = [[
+		Ты внимательно смотришь на глашатая. Этот картавый коршун занят работой.
+	]];
+}
+
+regimenstrasse_singer = obj {
+	nam = 'Менестрель';
+	dsc = [[
+		Какой-то {другой чёрт} орёт ему в ухо, что он лах.
+	]];
+	act = function()
+		walk 'regimenstrasse_conflict'
+	end;
+}
+
+regimenstrasse_singer_silent = obj {
+	nam = 'Менестрель';
+	dsc = [[
+		{Менестрель} играет на своей виолончели.
+	]];
+	act = [[
+		Ты внимательно смотришь на глашатая. Тот нежно гладит струны,
+		видимо, размышляя о чём-то своём.
+	]];
 }
