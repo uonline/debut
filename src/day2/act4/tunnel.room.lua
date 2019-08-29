3bc6dc7f2fc9688da43a3aecc4dc9b0dfa1d3a03
FaNatizM <whitelight92@mail.ru>
2019-08-29 09:08:45 +0300

    [D2A4] Add somne logic: структура локации Тоннель с тупиком

--- src/day2/act4/tunnel.room.lua
+++ src/day2/act4/tunnel.room.lua
@@ -1,21 +1,58 @@
+-- Переменные локации
 _tunnel_letsrock = false
+
+-- События
 on_event('ready to rock the boat', function()
 	_tunnel_letsrock = true
+	-- Удаляем темноту
+	objs('tunnel'):del('tunnel_darkness');
+	-- Добавляем главаря воров и полукровку
+	objs('tunnel'):add('tunnel_leader');
+	objs('tunnel'):add('tunnel_halfblood');
 end)
 
+-- Локация
 tunnel = room {
 	nam = 'Тоннель';
-	dsc = [[
-		Ты стоишь в очередном сраном тоннеле.
-	]];
+	dsc = function()
+		if _tunnel_letsrock then
+			return [[
+				Пройдя по тоннелю некоторое время в полной темноте, ты
+				в нерешительности останавливаешься.
+			]];
+		else
+			return [[
+				Ты видишь впереди тусклый огонёк.
+				^
+				Test
+				^
+				Стол, свеча, пара стульев, карта и какие-то ящики.
+			]];
+		end;
+	end;
 	obj = {
-		'tunnel_leader';
+		'tunnel_darkness'
 	};
 	way = {
 		'guild_camp';
 	};
 }
 
+-- Объекты
+-- Темнота
+tunnel_darkness = obj {
+	nam = 'Темнота';
+	dsc = [[
+		Кажется, что ты растворяешься в ней.
+	]];
+	act = function()
+		return [[
+			Армейская байка о культах и колодцах...
+		]];
+	end;
+}
+
+-- Главарь подполья
 tunnel_leader = obj {
 	nam = 'Главарь';
 	dsc = [[
@@ -40,5 +77,19 @@ tunnel_leader = obj {
 			-- Насколько ты охвачен течением, и что за сила тобой движет? Не использует ли тебя какая-либо
 			-- сила? Сумеешь ли ты отвести Рок?
 		]]
+	end;
+}
+
+-- Полукровка
+tunnel_halfblood = obj {
+	nam = 'Полукровка';
+	dsc = [[
+		Видели или не видели до этого?
+	]];
+	act = function()
+		-- Анализируем видели или не видели её до этого
+		return [[
+			...
+		]];
 	end;
 }
