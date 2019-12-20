b12eca61ca821a5bd79edee0b1c2b1f38a90c0c3
FaNatizM <whitelight92@mail.ru>
2019-12-20 07:57:40 +0300

    [D2A5] Add some logic: заготовки реплик диалога с чёрной птицей

--- src/day2/act5/stage4/sentry_memories.dlg.lua
+++ src/day2/act5/stage4/sentry_memories.dlg.lua
@@ -16,23 +16,81 @@ sentry_black_bird_dlg = dlg {
 		{
 			tag = 'Kevraza_and_first_vicar';
 			true;
-			'?';
+			'1.';
 			[[
 				...
 			]];
 			function()
-				_tower_stage2_where_is_halfblood = false;
-				tower_stage2_thieves_leader_dlg:pon('new_way');
 			end;
 		};
 		-- Главарь подполья и Полукровка про Религию Благих
+		{
+			tag = 'Thieves_leader_and_Halfblood_about_relion';
+			true;
+			'2.';
+			[[
+				...
+			]];
+			function()
+			end;
+		};
 		-- Птица цитирует стих из воспоминаний ГГ в пещере
+		{
+			tag = 'Cave_memories';
+			true;
+			'3.';
+			[[
+				...
+			]];
+			function()
+			end;
+		};
 
 		-- Расширенные реплики:
 		-- Если игрок прослушал все диалоги с крысой: диалог мага-крысы и первого городского наместника
+		{
+			tag = 'Mage_and_vicar';
+			true;
+			'4.';
+			[[
+				...
+			]];
+			function()
+			end;
+		};
 		-- Если игрок прочитал все воспоминания ГГ о документах капитана: диалог Кевразы и капитана
+		{
+			tag = 'Kevraza_and_captain';
+			true;
+			'5.';
+			[[
+				...
+			]];
+			function()
+			end;
+		};
 		-- Если игрок не взаимодействовал с Полукровкой в лагере гильдии: диалог главаря подполья и Полукровки
+		{
+			tag = 'Thieves_leader_and_Halfblood';
+			true;
+			'6.';
+			[[
+				...
+			]];
+			function()
+			end;
+		};
 		-- Если игрок выполнил все предыдущие условия: птица цитирует пятое четверостишье стиха из воспоминаний ГГ в пещере
+		{
+			tag = 'Secret_cave_memories';
+			true;
+			'7.';
+			[[
+				...
+			]];
+			function()
+			end;
+		};
 
 		-- Уходишь
 		{
