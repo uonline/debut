bc8f5dd0331b3615b30e4aa0c3387bb13be3261c
FaNatizM <whitelight92@mail.ru>
2020-04-14 13:53:24 +0300

    [D2A5] Add some text and logic: структура четвёртого этажа башни

--- src/day2/act5/stage4/stage4.room.lua
+++ src/day2/act5/stage4/stage4.room.lua
@@ -1,19 +1,8 @@
+-- Перходы
+-- Переход на 5 этаж
 tower_stage4_to_stage5 = vroom('Лестница', 'tower_stage5')
 tower_stage4_to_stage5:disable()
 
-tower_stage4 = room {
-	nam = 'Башня';
-	dsc = [[
-		На этаже есть лестница. Рядом с ней на стене странные символы.
-	]];
-	way = {
-		'tower_stage4_sentry';
-		'tower_stage4_back_to_stage2';
-		tower_stage4_to_stage5;
-	};
-}
-
--- Переходы
 -- Возврат на второй этаж
 tower_stage4_back_to_stage2 = room {
 	nam = 'Лестница';
@@ -25,8 +14,49 @@ tower_stage4_back_to_stage2 = room {
 	end;
 }
 
+-- События локации
 on_event('learned stairway symbols', function()
 	tower_stage4_to_stage5:enable()
 	tower_stage4_back_to_stage2:disable()
 end)
 
+-- Локация
+tower_stage4 = room {
+	nam = 'Башня';
+	dsc = [[
+		В очередной раз ты поднимаешься по лестнице, чтобы оказаться на этаж выше.
+	]];
+	obj = {
+		'tower_stage4_symbols';
+		'tower_stage4_windows';
+	};
+	way = {
+		'tower_stage4_sentry';
+		'tower_stage4_back_to_stage2';
+		tower_stage4_to_stage5;
+	};
+}
+
+-- Объекты локации
+-- Символы
+tower_stage4_symbols = obj {
+	nam = 'Окна';
+	dsc = [[
+		В отличие от предыдущих этажей, здесь низкие потолки.
+		На этаже есть лестница. Рядом с ней на стене {странные символы}.
+	]];
+	act = [[
+		Мягкая темнота. Тёплые лучи солнца пятнами падают на стены и пол.
+	]];
+}
+
+-- Окна
+tower_stage4_windows = obj {
+	nam = 'Окна';
+	dsc = [[
+		{Окна}
+	]];
+	act = [[
+		Мягкая темнота. Тёплые лучи солнца пятнами падают на стены и пол.
+	]];
+}
