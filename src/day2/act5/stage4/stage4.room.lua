9bea12b2008067509b3648c0965a304ce3e6b27c
FaNatizM <whitelight92@mail.ru>
2020-04-14 08:46:10 +0300

    [D2A5] Fix some logic: восстановлен нормальный переход на 2 и 5 этажи

--- src/day2/act5/stage4/stage4.room.lua
+++ src/day2/act5/stage4/stage4.room.lua
@@ -1,4 +1,3 @@
-tower_stage4_to_stage2 = vroom('Лестница', 'tower_stage2')
 tower_stage4_to_stage5 = vroom('Лестница', 'tower_stage5')
 tower_stage4_to_stage5:disable()
 
@@ -9,7 +8,6 @@ tower_stage4 = room {
 	]];
 	way = {
 		'tower_stage4_sentry';
-		--tower_stage4_to_stage2;
 		'tower_stage4_back_to_stage2';
 		tower_stage4_to_stage5;
 	};
@@ -29,6 +27,6 @@ tower_stage4_back_to_stage2 = room {
 
 on_event('learned stairway symbols', function()
 	tower_stage4_to_stage5:enable()
-	tower_stage4_to_stage2:disable()
+	tower_stage4_back_to_stage2:disable()
 end)
 
