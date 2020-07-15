8fdc1fe0fc852a76b7cc55c73c3fd5a2538df05e
FaNatizM <whitelight92@mail.ru>
2020-07-15 13:48:32 +0300

    [D2A7] Add some logic:      объекты финальной битвы

--- src/day2/act7/burning_quarter_part_II.room.lua
+++ src/day2/act7/burning_quarter_part_II.room.lua
@@ -4,7 +4,6 @@
 
 -- Переходы локации
 burning_quarter_to_lane_room = vroom('Переулок', 'lane_room');
---burning_quarter_to_lane_room:disable();
 
 -- Локация
 burning_quarter_fight = room {
@@ -13,8 +12,17 @@ burning_quarter_fight = room {
 		Финальная битва...
 	]];
 	obj = {
-		'burning_quarter_corpses_shifted';
-		-- Четырёхгранный кинжал проповедника
+		'burning_quarter_priest';
+		'burning_quarter_dagger';
+		'burning_quarter_ring';
+		'burning_quarter_corpses_armed';
+		'burning_quarter_knuckle';
+		'burning_quarter_zombie_fighter';
+		'burning_quarter_knife';
+		'burning_quarter_zombie_thug';
+		'burning_quarter_halberd';
+		'burning_quarter_zombie_guard';
+		'burning_quarter_godchosen';
 	};
 	enter = function()
 		return [[
@@ -28,12 +36,139 @@ burning_quarter_fight = room {
 }
 
 -- Объекты локации
--- TODO
---
--- Удар молотом, который проповедник легко останавливает рукой.
---
--- Червь, ничтожество, которое несмотря на это ставит себя в центр вселенной.
---
--- Ты когда-нибудь бывал на дне колодца. Когда весь мир сжимается в пятно белого
--- света в кольце тьмы?
+-- Проповедник
+burning_quarter_priest = obj {
+	nam = 'Проповедник';
+	dsc = [[
+		{Проповедник}.
+	]];
+	act = function()
+		return [[
+			-- Червь, ничтожество, которое несмотря на это ставит себя в центр вселенной.
+			^
+			-- Ты когда-нибудь бывал на дне колодца. Когда весь мир сжимается в пятно белого
+			света в кольце тьмы?
+		]];
+	end;
+}
 
+-- Четырёхгранный кинжал проповедника
+burning_quarter_dagger = obj {
+	nam = 'Кинжал проповедника';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Кольцо проповедника
+burning_quarter_ring = obj {
+	nam = 'Кольцо';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Трупы
+burning_quarter_corpses_armed = obj {
+	nam = 'Трупы с оружием';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Кастет урук-хай
+burning_quarter_knuckle = obj {
+	nam = 'Кастет урук-хай';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Зомби боец урук-хай
+burning_quarter_zombie_fighter = obj {
+	nam = 'Зомби боец урук-хай';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+			О бойцах урук-хай.
+		]];
+	end;
+}
+
+-- Нож головореза урук
+burning_quarter_knife = obj {
+	nam = 'Нож головореза урук';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Зомби головорез урук
+burning_quarter_zombie_thug = obj {
+	nam = '';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Алебарда
+burning_quarter_halberd = obj {
+	nam = 'Алебарда';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Зомби стражник
+burning_quarter_zombie_guard = obj {
+	nam = 'Зомби стражник';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
+
+-- Богоизбранный
+burning_quarter_godchosen = obj {
+	nam = '';
+	dsc = [[
+		{}.
+	]];
+	act = function()
+		return [[
+		]];
+	end;
+}
