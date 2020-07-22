fbc950e392b6033d174a86361aa09f0db7695f80
m1kc (Max Musatov) <m1kc@yandex.ru>
2020-07-22 08:09:08 +0300

    [D2A8] First draft

--- src/day2/act8/burning_quarter_part_II.gameover.lua
+++ src/day2/act8/burning_quarter_part_II.gameover.lua
@@ -1,6 +1,9 @@
-killed_by_priest = room {
+burning_quarter_part_II_gameover = room {
 	nam = 'Игра окончена';
 	dsc = [[
 		Темнота более не отпускает тебя.
 	]];
+	enter = function()
+		inv():zap()
+	end;
 }
