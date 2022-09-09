2ab4eeb3f91ae5fd85ebed18d182f0703349a9f9
FaNat1zM <kerrok.dir@ya.ru>
2022-09-09 21:52:25 +0300

    fix /global:      code style.

--- src/global/vars.lua
+++ src/global/vars.lua
@@ -1,2 +1,5 @@
-_d1a1_water_problem_quest = false -- признак, взят ли квест на воду в деревне
+ -- признак, взят ли квест на воду в деревне
+_d1a1_water_problem_quest = false
+
+ -- признак, избит ли кузнец
 _beaten_blacksmith = false
